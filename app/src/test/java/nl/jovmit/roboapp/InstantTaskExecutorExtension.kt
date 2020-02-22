32863665e2ae10ec69fead2a12ccce88e93e2c13
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-02-22 09:08:39 +0100

    Dependency updates

--- app/src/test/java/nl/jovmit/roboapp/InstantTaskExecutorExtension.kt
+++ app/src/test/java/nl/jovmit/roboapp/InstantTaskExecutorExtension.kt
@@ -2,6 +2,10 @@ package nl.jovmit.roboapp
 
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.arch.core.executor.TaskExecutor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtensionContext
 class InstantTaskExecutorExtension : BeforeEachCallback, AfterEachCallback {
 
     override fun beforeEach(context: ExtensionContext) {
+        Dispatchers.setMain(TestCoroutineDispatcher())
         ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
             override fun executeOnDiskIO(runnable: Runnable) {
                 runnable.run()
@@ -26,5 +31,6 @@ class InstantTaskExecutorExtension : BeforeEachCallback, AfterEachCallback {
 
     override fun afterEach(context: ExtensionContext?) {
         ArchTaskExecutor.getInstance().setDelegate(null)
+        Dispatchers.resetMain()
     }
 }
