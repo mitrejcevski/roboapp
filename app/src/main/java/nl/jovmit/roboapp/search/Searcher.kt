7df569c651174a9f1c4bd080576f84f12c344d84
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-11-28 01:49:28 +0100

    Performing the search in the background thread

--- app/src/main/java/nl/jovmit/roboapp/search/Searcher.kt
+++ app/src/main/java/nl/jovmit/roboapp/search/Searcher.kt
@@ -2,12 +2,17 @@ package nl.jovmit.roboapp.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import nl.jovmit.roboapp.search.data.SearchState
 
 class Searcher(
   private val validator: Validator,
   private val repository: SearchRepository
-) {
+) : ViewModel() {
 
   private val _liveData = MutableLiveData<SearchState>()
   val searchLiveData: LiveData<SearchState> = _liveData
@@ -22,7 +27,12 @@ class Searcher(
 
   private fun proceedSearch(query: String) {
     _liveData.value = SearchState.ShowLoading
-    _liveData.value = repository.search(query)
-    _liveData.value = SearchState.HideLoading
+    viewModelScope.launch {
+      val results = withContext(Dispatchers.IO) {
+        repository.search(query)
+      }
+      _liveData.value = results
+      _liveData.value = SearchState.HideLoading
+    }
   }
 }
