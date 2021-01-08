1be362edd55e90fd9f91db9e06a3d772201e34b7
Jov Mit <mitrejcevski@gmail.com>
2021-01-09 00:00:50 +0100

    Returns empty result when no values matching the query

---
+++ app/src/test/java/nl/jovmit/roboapp/search/InMemorySearchServiceTest.kt
@@ -0,0 +1,23 @@
+package nl.jovmit.roboapp.search
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class InMemorySearchServiceTest {
+
+  @Test
+  fun returnEmptyResult() {
+    val searchService = searchServiceWithout(
+      listOf("Item 1", "item 2")
+    )
+
+    val result = searchService.findMatches("item")
+
+    assertEquals(emptyList<String>(), result)
+  }
+
+  private fun searchServiceWithout(availableValues: List<String>) =
+    InMemorySearchService(
+      availableValues.map { it.reversed() }
+    )
+}
